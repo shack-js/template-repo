4f5941453f499d7c24b1078af223fd9afe60f28b
josh <postor@gmail.com>
2021-06-15 21:06:12 +0800

    basic

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,10 +1,10 @@
 import tpl from '../index.mjs'
-import { join, resolve, dirname } from 'path'
+import { join } from 'path'
 import fs from 'fs-extra'
 
 test('mainly works', async () => {
   const name = 'test-app'
-  let target = join(resolve(dirname('')), name)
+  let target = `./${name}`
   await tpl({ name }, `https://github.com/shack-js/template-basic.git`, target)
   let pkg = await fs.readJSON(join(target, 'package.json'))
   expect(pkg.name).toBe(name)
